19887e40fa2c4c9fe37886395d25ed930fd8dc37
keiko kondo <keiko.ayaka@icloud.com>
2020-11-17 15:28:59 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,8 @@ Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Proc.routes.draw do
+#   resources :blogs
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,9 @@
-rrequire 'fileutils'
-listen '/tmp/nginx.socket'
+require 'fileutils'
+preload_app true
+timeout 5
+worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
+
 before_fork do |server,worker|
 	FileUtils.touch('/tmp/app-initialized')
 end
